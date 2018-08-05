7d64c81c33256def1666c1645c42e3119b70e3e7
Dor Bar <dor4231@gmail.com>
2018-08-06 01:14:19 +0300

    feat: Can remove a contact from the list

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react'
 
 
-class ListContacts extends Component {
-    render() {
-        const contacts = this.props.contacts;
-        return (
-            <ol className='contact-list'>
-                {contacts.map(person => (
-                    <li key={person.id} className='contact-list-item'>
-                    <div className='contact-avatar' style={{
-                        backgroundImage: `url(${person.avatarURL})`
-                    }}/>
-                    <div className='contact-details'>
-                        <p>{person.name}</p>
-                        <p>{person.email}</p>
-                    </div>
-                        <button className='contact-remove'>
-                            Remove
-                        </button>
-                    </li>
-                ))}
-            </ol>
-        )
-    }
+function ListContacts(props) {
+    return (
+        <ol className='contact-list'>
+            {props.contacts.map(person => (
+                <li key={person.id} className='contact-list-item'>
+                <div className='contact-avatar' style={{
+                    backgroundImage: `url(${person.avatarURL})`
+                }}/>
+                <div className='contact-details'>
+                    <p>{person.name}</p>
+                    <p>{person.email}</p>
+                </div>
+                    <button onClick={() => props.onDeleteContact(person)} className='contact-remove'>
+                        Remove
+                    </button>
+                </li>
+            ))}
+        </ol>
+    )
 }
 
 
