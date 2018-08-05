8d3a25d0c37a5dd9430c5d78865f009cf33e781c
Dor Bar <dor4231@gmail.com>
2018-08-05 23:12:49 +0300

    feat: Create Contact List component and use it a couple of times

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class ContactList extends Component {
+  render() {
+    const people = this.props.contacts;
+
+    return <ol>
+      {people.map((person) => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+      </ol>
+  }
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ContactList contacts={[
+          {name: "Dor"},
+          {name: "Amir"},
+          {name: "Aviv"}
+        ]}/>
+        <ContactList contacts={[
+          {name: "Amira"},
+          {name: "Avivit"},
+          {name: "Dorit"}
+        ]}/>
+      </div>
+    );
+  }
+}
+
+export default App;
